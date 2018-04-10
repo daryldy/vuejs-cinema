0becd95481c1897744aaaf9112dea60a8c52ed81
Daryl Daly <dwdaly@yahoo.com>
2018-04-09 21:11:27 -0700

    End of lecture 80

--- src/main.js
+++ src/main.js
@@ -5,20 +5,51 @@ import genres from './util/genres'
 
 new Vue({
   el: "#app",
+  data: {
+    genre: [],
+    time: [],
+  },
+  methods: {
+    checkFilter(category,title,checked) {
+      if (checked) {
+        this[category].push(title)
+      } else {
+        let index = this[category].indexOf(title)
+        if (index >= 0) {
+          this[category].splice(index,1)
+        }
+      }
+    }
+  },
   components: {
     'movie-list': {
       template: `<div id="movie-list">
-                    <div v-for="movie in movies" class="movie" >{{ movie.title }}</div>
+                    <div v-for="movie in filteredMovies" class="movie" >{{ movie.title }}</div>
                  </div>`,
       data() {
         return {
 	  movies: [
-            { title: 'movie 1' },
-	    { title: 'movie 2' },
-	    { title: 'movie 3' },
+            { title: 'movie 1', genre: genres.CRIME },
+	    { title: 'movie 2', genre: genres.COMEDY },
+	    { title: 'movie 3', genre: genres.COMEDY },
 	  ]
 	}
-      }
+      },
+      props: [ 'genre','time' ],
+      methods: {
+        moviePassesGenreFilter(movie) {
+          if (!this.genre.length) {
+            return true
+          } else {
+            return this.genre.find(genre => movie.genre === genre)
+          }
+        }
+      },
+      computed: {
+        filteredMovies() {
+          return this.movies.filter(this.moviePassesGenreFilter);
+        },
+      },
     },
     'movie-filter': {
       data() {
@@ -33,8 +64,8 @@ new Vue({
 		    </div>
                  </div>`,
       methods: {
-        checkFilter(e) {
-	  console.log('checkFilter ' + e.title + ' ' + e.checked)
+        checkFilter(category,title,checked) {
+          this.$emit('check-filter',category,title,checked)
 	}
       },
       components: {
@@ -54,7 +85,7 @@ new Vue({
 	  methods: {
 	    checkFilter() {
 	      this.checked = !this.checked
-	      this.$emit('check-filter',{ title: this.title, checked:this.checked })
+	      this.$emit('check-filter', 'genre', this.title, this.checked)
 	    },
 	  },
 	}
